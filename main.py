e86415b1769cf17e04f4c7a8a2604da6eb93b486
antoloyola90 <antoloyola90@gmail.com>
2015-12-10 23:52:16 -0500

    working on displaying resource

--- main.py
+++ main.py
@@ -42,34 +42,6 @@ class MainPage(webapp2.RequestHandler):
         startTimeGet = self.request.get('startTimeGet')
         endTimeGet = self.request.get('endTimeGet')
         tagsGet = self.request.get('tagsGet')
-        resources_for_user = Resource.gql("WHERE user = :1", user)
-        index_list = list(resources_for_user)
-
-        if not( resourceNameGet == "" ):
-            index_list.append(Resource(user=user, name=resourceNameGet, startTime=startTimeGet, endTime=endTimeGet, tags=tagsGet))
-
-        template_values = {
-            'user': user,
-            'url': url,
-            'url_linktext': url_linktext,
-            'resources_for_user': index_list,
-        }
-        
-        template = JINJA_ENVIRONMENT.get_template('index.html')
-        self.response.write(template.render(template_values))
-
-class MainPage(webapp2.RequestHandler):
-    def get(self):
-        user = users.get_current_user()
-        url = users.create_logout_url(self.request.uri)
-        url_linktext = 'Logout'
-        if user is None:
-            self.redirect(users.create_login_url(self.request.uri))
-
-        resourceNameGet = self.request.get('resourceNameGet')
-        startTimeGet = self.request.get('startTimeGet')
-        endTimeGet = self.request.get('endTimeGet')
-        tagsGet = self.request.get('tagsGet')
         sizeGet = self.request.get('sizeGet')
 
         if not( sizeGet == "" ):
@@ -135,7 +107,12 @@ class ViewResource(webapp2.RequestHandler):
         if user is None:
             self.redirect(users.create_login_url(self.request.uri))
 
-        resourceNameGet = self.request.get('resourceName')
+        resourceID = self.request.get('id')
+        resource_by_id = Resource.gql("WHERE ID = :1", resourceID)
+
+        for e in resource_by_id:
+            logging.info('e.name = ' + e.name)
+
 
 
 app = webapp2.WSGIApplication([
