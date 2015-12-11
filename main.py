be1e8b63174988b06a42db88097da5a4bdd9a61d
antoloyola90 <antoloyola90@gmail.com>
2015-12-10 21:52:24 -0500

    Main page and adding a resource

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# [START imports]
 import os
 import urllib
 
@@ -8,97 +7,139 @@ from google.appengine.ext import ndb
 import jinja2
 import webapp2
 
+import datetime
+import time
+import logging
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
-# [END imports]
-
-DEFAULT_GUESTBOOK_NAME = 'default_guestbook'
 
 
-# We set a parent key on the 'Greetings' to ensure that they are all
-# in the same entity group. Queries across the single entity group
-# will be consistent. However, the write rate should be limited to
-# ~1/second.
+class Reservations(ndb.Model):
+    reservedBy = ndb.StringProperty(indexed=False)
+    startTime = ndb.TimeProperty(indexed=False)
+    endTime = ndb.TimeProperty(indexed=False)
 
-def guestbook_key(guestbook_name=DEFAULT_GUESTBOOK_NAME):
-    """Constructs a Datastore key for a Guestbook entity.
-    We use guestbook_name as the key.
-    """
-    return ndb.Key('Guestbook', guestbook_name)
+class Resource(ndb.Model):
+    user = ndb.UserProperty()
+    name = ndb.StringProperty(indexed=False)
+    startTime = ndb.StringProperty(indexed=False)
+    endTime = ndb.StringProperty(indexed=False)
+    tags = ndb.StringProperty(indexed=False)
+    reservations = ndb.StructuredProperty(Reservations, repeated = True)
 
-
-class Author(ndb.Model):
-    """Sub model for representing an author."""
-    identity = ndb.StringProperty(indexed=False)
-    email = ndb.StringProperty(indexed=False)
-
-
-class Greeting(ndb.Model):
-    """A main model for representing an individual Guestbook entry."""
-    author = ndb.StructuredProperty(Author)
-    content = ndb.StringProperty(indexed=False)
-    date = ndb.DateTimeProperty(auto_now_add=True)
-
-
-# [START main_page]
 class MainPage(webapp2.RequestHandler):
-
     def get(self):
-        guestbook_name = self.request.get('guestbook_name',
-                                          DEFAULT_GUESTBOOK_NAME)
-        greetings_query = Greeting.query(
-            ancestor=guestbook_key(guestbook_name)).order(-Greeting.date)
-        greetings = greetings_query.fetch(10)
-
         user = users.get_current_user()
-        if user:
-            url = users.create_logout_url(self.request.uri)
-            url_linktext = 'Logout'
-        else:
-            url = users.create_login_url(self.request.uri)
-            url_linktext = 'Login'
+        url = users.create_logout_url(self.request.uri)
+        url_linktext = 'Logout'
+        if user is None:
+            self.redirect(users.create_login_url(self.request.uri))
+
+        resourceNameGet = self.request.get('resourceNameGet')
+        startTimeGet = self.request.get('startTimeGet')
+        endTimeGet = self.request.get('endTimeGet')
+        tagsGet = self.request.get('tagsGet')
+        resources_for_user = Resource.gql("WHERE user = :1", user)
+        index_list = list(resources_for_user)
+
+        if not( resourceNameGet == "" ):
+            index_list.append(Resource(user=user, name=resourceNameGet, startTime=startTimeGet, endTime=endTimeGet, tags=tagsGet))
 
         template_values = {
             'user': user,
-            'greetings': greetings,
-            'guestbook_name': urllib.quote_plus(guestbook_name),
             'url': url,
             'url_linktext': url_linktext,
+            'resources_for_user': index_list,
         }
-
+        
         template = JINJA_ENVIRONMENT.get_template('index.html')
         self.response.write(template.render(template_values))
-# [END main_page]
 
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        user = users.get_current_user()
+        url = users.create_logout_url(self.request.uri)
+        url_linktext = 'Logout'
+        if user is None:
+            self.redirect(users.create_login_url(self.request.uri))
 
-class Guestbook(webapp2.RequestHandler):
+        resourceNameGet = self.request.get('resourceNameGet')
+        startTimeGet = self.request.get('startTimeGet')
+        endTimeGet = self.request.get('endTimeGet')
+        tagsGet = self.request.get('tagsGet')
+        sizeGet = self.request.get('sizeGet')
 
+        if not( sizeGet == "" ):
+            sizeInt = int(sizeGet)
+            logging.info('sizeInt = ' + str(sizeInt))
+
+        resources_for_user = Resource.gql("WHERE user = :1", user)
+        index_list = list(resources_for_user)
+        
+        logging.info('len(index_list) = ' + str(len(index_list)))
+
+        if not( resourceNameGet == "" ) and sizeInt == len(index_list):
+            index_list.append(Resource(user=user, name=resourceNameGet, startTime=startTimeGet, endTime=endTimeGet, tags=tagsGet))
+
+        index_list.sort(key=lambda x: x.name)
+        template_values = {
+            'user': user,
+            'url': url,
+            'url_linktext': url_linktext,
+            'resources_for_user': index_list,
+        }
+        
+        template = JINJA_ENVIRONMENT.get_template('index.html')
+        self.response.write(template.render(template_values))
+
+class AddResource(webapp2.RequestHandler):
+    def get(self):
+        user = users.get_current_user()
+        if user is None:
+            self.redirect(users.create_login_url(self.request.uri))
+        template = JINJA_ENVIRONMENT.get_template('addResource.html')
+        template_values = {}
+        self.response.write(template.render(template_values))
     def post(self):
-        # We set the same parent key on the 'Greeting' to ensure each
-        # Greeting is in the same entity group. Queries across the
-        # single entity group will be consistent. However, the write
-        # rate to a single entity group should be limited to
-        # ~1/second.
-        guestbook_name = self.request.get('guestbook_name',
-                                          DEFAULT_GUESTBOOK_NAME)
-        greeting = Greeting(parent=guestbook_key(guestbook_name))
+        resourceNameGet = self.request.get('resourceName')
+        startTimeGet = self.request.get('startTime')
+        endTimeGet = self.request.get('endTime')
+        tagsGet = self.request.get('tags')
+        userGet = users.get_current_user()
 
-        if users.get_current_user():
-            greeting.author = Author(
-                    identity=users.get_current_user().user_id(),
-                    email=users.get_current_user().email())
+        repeatedResource = False
+        resources_for_user = Resource.gql("WHERE user = :1", userGet)
+        for e in resources_for_user:
+            if e.name == resourceNameGet:
+                repeatedResource = True
+                break
+        
+        index_list = list(resources_for_user)
+        if repeatedResource:
+            template = JINJA_ENVIRONMENT.get_template('addResource.html')
+            template_values = {
+                'repeatedResource': repeatedResource,
+            }
+            self.response.write(template.render(template_values))
+        else:
+            resource = Resource(user=userGet, name=resourceNameGet, startTime=startTimeGet, endTime=endTimeGet, tags=tagsGet)
+            resource.put()
+            self.redirect('/?resourceNameGet='+resourceNameGet+'&startTimeGet='+startTimeGet+'&endTimeGet='+endTimeGet+'&tagsGet='+tagsGet+'&sizeGet='+str(len(index_list)))
 
-        greeting.content = self.request.get('content')
-        greeting.put()
+class ViewResource(webapp2.RequestHandler):
+    def get(self):
+        user = users.get_current_user()
+        if user is None:
+            self.redirect(users.create_login_url(self.request.uri))
 
-        query_params = {'guestbook_name': guestbook_name}
-        self.redirect('/?' + urllib.urlencode(query_params))
+        resourceNameGet = self.request.get('resourceName')
 
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
-    ('/sign', Guestbook),
+    ('/addResource', AddResource),
+    ('/resource', ViewResource)
 ], debug=True)
